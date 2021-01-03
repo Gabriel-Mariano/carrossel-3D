a4cdf08e8332aad91bc3b8e21327436d4e8ddbcd
Gabriel Mariano <gabrielmarianoo93@gmail.com>
2021-01-03 18:30:23 -0300

    Estruturando o Carrossel-3D

---
+++ src/components/card/card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './card.css';
+
+function Card(){
+    return(
+        <div className="card-person">
+            
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/components/main/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './main.css';
+
+function Carousel(){
+    return(
+        <div className="container">
+            <div className="carousel">
+                { /* conteúdo do carrossel */}
+                
+            </div>
+        </div>
+    );
+}
+
+export default Carousel;
